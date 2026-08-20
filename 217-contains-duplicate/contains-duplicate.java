5a51a815c3a71be8ccd525309b016b3ac0132ca2
Diwakar655 <diwakarmaurya933@gmail.com>
2026-08-20 10:54:00 +0530

    Time: 24 ms (28.52%) | Memory: 81.3 MB (91.74%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,15 +1,14 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        boolean bool =false;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] == nums[i+1]){
-                 bool =true;
+
+    public boolean containsDuplicate(int[] arr) { 
+        Arrays.sort(arr);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] == arr[i-1]){
+                return true;
             }
         }
+        return false;
 
-        return bool;
-
+        }
         
     }
-}
